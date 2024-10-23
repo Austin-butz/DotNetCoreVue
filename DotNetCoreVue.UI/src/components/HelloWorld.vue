ae6e7fcc3d338434c2af00a06df1b1a03c00bf42
Austin Butz <Austintrueland@gmail.com>
2024-10-23 18:42:02 -0400

    init

---
+++ DotNetCoreVue.UI/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello, world!</h1>
+</template>
+
+<script>
+   export default {
+       name: 'HelloWorld'
+   }
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        color: red;
+    }
+</style>
